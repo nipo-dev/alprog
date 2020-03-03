31d37d1ca64f239dd9b4dd66a36ee1fb7635f7fc
nipo-dev <hanif.irsyad8@gmail.com>
2020-03-03 16:37:57 +0700

    nambahin pelajaran hari ini

---
+++ Pengulangan While.py
@@ -0,0 +1,9 @@
+jawab = 'ya'
+hitung = 0
+
+while(True):
+    hitung += 1
+    jawab = input("Ulang lagi tidak? ")
+    if jawab == 'tidak':
+        break
+print ("total ngulang " + str(hitung))
---
+++ range.py
@@ -0,0 +1,5 @@
+for i in range (0,4):
+ print (i)
+konfirmasi = ["yaa","tidak","oke"]
+for x in konfirmasi:
+     print (x)
